1e8329ada1413fc387e70bcf31fd77a25a89f9ed
Andre Ferreira <andre.dev.linux@gmail.com>
2024-03-08 18:51:56 -0300

    Initial commit

---
+++ src/main/java/andrelsf/github/com/mcaccounts/McAccountsApplication.java
@@ -0,0 +1,13 @@
+package andrelsf.github.com.mcaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrelsf/github/com/mcaccounts/McAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package andrelsf.github.com.mcaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
